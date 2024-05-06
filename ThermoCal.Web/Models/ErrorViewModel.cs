cac2fb6593d4d6d1d5375986e6f7da7bd5af07c2
Serhat Tomak <serhattomak@gmail.com>
2024-05-06 23:57:50 +0300

    Add project files.

---
+++ ThermoCal.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ThermoCal.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
